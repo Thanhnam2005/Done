e79fb7bf8f676c181206ff85b82fed4c3a858374
Thanhnam2005 <103724345+Thanhnam2005@users.noreply.github.com>
2024-11-22 19:44:27 +0700

    done

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/image-to-pdf/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/image-to-pdf' : '',
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/image-to-pdf' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/image-to-pdf' : '',
 }
 
 module.exports = nextConfig
